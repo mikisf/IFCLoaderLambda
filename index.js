713bd1e6f499c7a3f1d18efb8a3050b01902ea0b
mikisf <mikisalafranci@gmail.com>
2023-04-02 19:10:52 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import * as THREE from 'three';
+import IFCLoader from "web-ifc-three/IFCLoader.js";
+import fs from 'fs'
+
+fs.readFile('src//example.ifc', 'utf8', (err, data) => {
+    const ifcLoader = new IFCLoader();
+    ifcLoader.load(data, function (model) {
+        //console.log(model.castShadow)
+    })
+});
+console.log("hi")
