a62fff684b215b1df4fe9e8f0936d8ea4c21905b
mikisf <mikisalafranci@gmail.com>
2023-05-08 17:06:50 +0200

    Change import ifc

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 import * as THREE from 'three';
 import IFCLoader from "web-ifc-three/IFCLoader.js";
-import fs from 'fs'
 
-fs.readFile('example.ifc', 'utf8', (err, data) => {
-    const ifcLoader = new IFCLoader();
-    ifcLoader.load(data, function (model) {
-        //console.log(model.castShadow)
-    })
-});
-console.log("hi")
+const ifcLoader = new IFCLoader();
+ifcLoader.load('example.ifc', function (model) {
+    console.log(model)
+})
